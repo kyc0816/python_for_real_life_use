b2ec8782d6c106a5c133b913fafc3966ccea9a54
justin <youngcheol33@gmail.com>
2022-10-09 01:10:15 +0900

    브라우저 자동 오픈 말고 그냥 링크만 생성해주는걸로 바꿈

---
+++ 실생활적용사례들/스카이스캐너/skyscanner_only_print.py
@@ -0,0 +1,25 @@
+# 이게 들어있는 폴더까지 cd해서 온 후
+# source ../../venv/bin/activate 해서 venv 켠 뒤
+# python skyscanner_only_print.py 해서 실행
+
+from datesGenerator import dataGenerator
+from savedUrls import urlFirst, urlLast
+from cities import cities
+import webbrowser
+
+dataGenerator = dataGenerator()
+datesCombinations = dataGenerator.datesCombinations
+
+def writeURLsFile(cityName, numDates):
+    stringToWrite = ""
+    urlFirst2 = urlFirst[cityName]
+    urlLast2 = urlLast[cityName]
+    for i in range(numDates):
+        stringToWrite += urlFirst2 + datesCombinations[i][0] + '/' + datesCombinations[i][1] + urlLast2 + "\n"
+    f = open(f'./static/{cityName}URLs.txt', 'w')
+    f.write(stringToWrite)
+    f.close()
+
+for city in cities:
+    writeURLsFile(city, len(datesCombinations))
+
